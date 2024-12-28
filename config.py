14d5b3917bec71a6eb247dbe0287010a7c50ced5
Radare <126122384+idmkdev@users.noreply.github.com>
2024-12-28 19:33:58 +0300

    я ща умру с этим конфигом разбираться

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-build_download=True # adb install on device after compile
-build_sign=True
-key_password="test" # create your own key and put this to key/keystore.jks
+build_download=False # adb install on device after compile
+build_sign=False
+key_password="None" # create your own key and put this to key/keystore.jks
 
