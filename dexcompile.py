eddf47c35289bb4823a21a51d9c6e0f1c366c7d5
radarearz <delovoyradare@gmail.com>
2024-12-28 19:21:38 +0300

    убрал пути адаптация под гит

--- dexcompile.py
+++ dexcompile.py
@@ -2,22 +2,20 @@ import os
 import sys
 import shutil
 import subprocess
+import config
 
 def compile_to_dex(src_dir):
     project_name = os.path.basename(os.path.normpath(src_dir))
+    current_directory = os.getcwd()
 
-    java_compiler = r"C:\\Program Files\\Java\\jdk-21.0.1\\bin\\javac.exe"
-    android_jar = r"C:\\Users\\dllll\\AppData\\Local\\Android\\Sdk\\platforms\\android-33\\android.jar"
     classpath = (
-        r"C:\\Users\\dllll\\AppData\\Local\\Android\\Sdk\\platforms\\android-33\\android.jar;"
-        r"C:\\Games\\radare\\java\\arzmob-classes\\classes-dex2jar.jar;"
-        r"C:\\Games\\radare\\java\\arzmob-classes\\classes2-dex2jar.jar;"
-        r"C:\\Games\\radare\\java\\arzmob-classes\\classes3-dex2jar.jar;"
-        r"C:\\Games\\radare\\java\\arzmob-classes\\classes4-dex2jar.jar;"
-        r"C:\\Games\\radare\\java\\arzmob-classes\\classes5-dex2jar.jar"
+        f"{os.path.join(current_directory, 'arzmob-classes', 'android.jar')};"
+        f"{os.path.join(current_directory, 'arzmob-classes', 'classes-dex2jar.jar')};"
+        f"{os.path.join(current_directory, 'arzmob-classes', 'classes2-dex2jar.jar')};"
+        f"{os.path.join(current_directory, 'arzmob-classes', 'classes3-dex2jar.jar')};"
+        f"{os.path.join(current_directory, 'arzmob-classes', 'classes4-dex2jar.jar')};"
+        f"{os.path.join(current_directory, 'arzmob-classes', 'classes5-dex2jar.jar')}"
     )
-    dx_tool = r"C:\\Users\\dllll\\AppData\\Local\\Android\\Sdk\\build-tools\\30.0.3\\dx"
-
     out_dir = src_dir + "\\out"
     dex_file = os.path.join(out_dir, f"{project_name}.dex")
 
@@ -35,14 +33,14 @@ def compile_to_dex(src_dir):
             raise FileNotFoundError("No .java files found in the source directory.")
 
         javac_command = (
-            f'"{java_compiler}" -source 8 -target 8 '
+            f'javac -source 8 -target 8 '
             f'-classpath "{classpath}" '
             f'-d {out_dir} ' + " ".join(f'"{java_file}"' for java_file in java_files)
         )
         print(f"Running: {javac_command}")
         subprocess.check_call(javac_command, shell=True)
 
-        dx_command = f'"{dx_tool}" --dex --output="{dex_file}" "{out_dir}"'
+        dx_command = f'dx --dex --output="{dex_file}" "{out_dir}"'
         print(f"Running: {dx_command}")
         subprocess.check_call(dx_command, shell=True)
 
